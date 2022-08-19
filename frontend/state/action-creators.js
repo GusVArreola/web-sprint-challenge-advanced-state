193242e178c52ed8be95fffc7cf7ed3f500abeeb
Gustavo Vega <gustavov.arreola@gmail.com>
2022-08-19 02:05:14 -0600

    Started sprint need to revise or watch videos

--- frontend/state/action-creators.js
+++ frontend/state/action-creators.js
@@ -1,38 +1,79 @@
+import * as types from './action-types'
+import axios from 'axios'
+
+const URL = 'http://localhost:9000/api/quiz/'
 // ❗ You don't need to add extra action creators to achieve MVP
-export function moveClockwise() { }
+export function moveClockwise(num) {
+return {type: types.MOVE_CLOCKWISE, payload: num}
+ }
 
-export function moveCounterClockwise() { }
+export function moveCounterClockwise(num) {
+  return {type: types.MOVE_COUNTERCLOCKWISE, payload: num}
+ }
 
-export function selectAnswer() { }
+export function selectAnswer(id) { 
+  return {type: types.SET_SELECTED_ANSWER, payload: id}
+}
 
-export function setMessage() { }
+export function setMessage(name) { 
+return ({ type: types.SET_INFO_MESSAGE, payload: name})
+}
 
-export function setQuiz() { }
+export function setQuiz(data) { 
+  return ({type: types.SET_QUIZ_INTO_STATE, payload: data})
+}
 
-export function inputChange() { }
+export function inputChange({ name, value }) {
+  return {
+    type: types.INPUT_CHANGE,
+    payload: { name, value }
+  }
+}
 
-export function resetForm() { }
+export function resetForm() { 
+  return({ type: types.RESET_FORM })
+}
 
 // ❗ Async action creators
 export function fetchQuiz() {
   return function (dispatch) {
     // First, dispatch an action to reset the quiz state (so the "Loading next quiz..." message can display)
+      axios.get(`${URL}next`)
+      .then((result) => {
+        dispatch({type: types.SET_QUIZ_INTO_STATE, payload: null})
+        dispatch({type: types.SET_QUIZ_INTO_STATE, payload: result.data})
+        
+        })
+      
+         .catch(err => console.log(err))
     // On successful GET:
     // - Dispatch an action to send the obtained quiz to its state
   }
 }
-export function postAnswer() {
+export function postAnswer(id, quizId) {
   return function (dispatch) {
     // On successful POST:
+    axios.post(`${URL}answer`, {quiz_id: quizId, answer_id: id})
+    .then((res) =>{
+      dispatch({type: types.SET_SELECTED_ANSWER, payload: null})
+      dispatch({type: types.SET_INFO_MESSAGE, payload: res.data.message})
+      dispatch(fetchQuiz())
+    })
+    .catch(err => console.log({err}))
     // - Dispatch an action to reset the selected answer state
     // - Dispatch an action to set the server message to state
     // - Dispatch the fetching of the next quiz
   }
 }
-export function postQuiz() {
+export function postQuiz(news, trues, falses) {
   return function (dispatch) {
     // On successful POST:
     // - Dispatch the correct message to the the appropriate state
+    axios.post(`${URL}new`, {question_text: news, true_answer_text: trues, false_answer_text: falses})
+    .then((res) => {
+      dispatch({type: types.SET_QUIZ_INTO_STATE, payload: res.data.data})
+    })
+    .catch(err => console.log(err))
     // - Dispatch the resetting of the form
   }
 }
--- frontend/state/reducer.js
+++ frontend/state/reducer.js
@@ -1,24 +1,49 @@
 // ❗ You don't need to add extra reducers to achieve MVP
 import { combineReducers } from 'redux'
+import * as type from './action-types'
 
 const initialWheelState = 0
 function wheel(state = initialWheelState, action) {
-  return state
+  switch(action.type){
+    case type.MOVE_CLOCKWISE: {
+      return state >= 5 ? state = initialWheelState : state + action.payload
+    }
+    case type.MOVE_COUNTERCLOCKWISE: {
+      return state <= 0 ? state = 5 : state - action.payload
+    }
+    default: return state
+  }
 }
 
-const initialQuizState = null
+const initialQuizState = []
 function quiz(state = initialQuizState, action) {
-  return state
+  switch(action.type){
+  case type.SET_QUIZ_INTO_STATE:{
+  return {...state, state: action.payload}
+  }
+  default: return state
+  }
 }
+
 
 const initialSelectedAnswerState = null
 function selectedAnswer(state = initialSelectedAnswerState, action) {
-  return state
+  switch(action.type){
+    case type.SET_SELECTED_ANSWER:
+      return action.payload
+
+      default: return state
+  }
+  
 }
 
 const initialMessageState = ''
 function infoMessage(state = initialMessageState, action) {
-  return state
+  switch(action.type){
+    case type.SET_INFO_MESSAGE:
+      return action.payload
+      default: return state
+  }
 }
 
 const initialFormState = {
@@ -27,7 +52,16 @@ const initialFormState = {
   newFalseAnswer: '',
 }
 function form(state = initialFormState, action) {
-  return state
+  switch(action.type){
+    case type.INPUT_CHANGE: {
+    const { name, value } = action.payload
+      return {...state, [name]: value }
+    }
+    case type.RESET_FORM:{
+      return state = initialFormState
+    }
+    default: return state
+  }
 }
 
 export default combineReducers({ wheel, quiz, selectedAnswer, infoMessage, form })
